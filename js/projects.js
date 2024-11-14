928e2a0ed5ccca5040041cd0d3b3bf12bdf0d8ea
James Marwell <jcmarwell@gmail.com>
2024-11-13 17:09:14 -0800

    Added objects

--- js/projects.js
+++ js/projects.js
@@ -2,45 +2,50 @@ export function getProjectList() {
     return {
         projects: [
             {
-                title: "Cube.001",
-                id: "cube_001",
-                group: "project 1",
-                modelURL: "/projects/cube_001/cube.glb",
+                title: "wallE",
+                id: "wallE",
+                group: "personal",
+                modelURL: "/projects/wallE/wallE.glb",
                 descriptionURL: "/projects/cube_001/desc.html",
                 imagesURL: "/projects/cube_001/images.html",
                 scale: {
-                    x: .5, y: .5, z: .5
+                    x: .005, y: .005, z: .005
+                }
+            },
+            {
+                title: "terminator_foot",
+                id: "terminator_foot",
+                group: "classwork",
+                modelURL: "/projects/terminator_foot/terminator_foot.glb",
+                descriptionURL: "/projects/cube_001/desc.html",
+                imagesURL: "/projects/cube_001/images.html",
+                scale: {
+                    x: .005, y: .005, z: .005
+                }
+            },
+            {
+                title: "cereal_container",
+                id: "cereal_container",
+                group: "classwork",
+                modelURL: "/projects/cereal_container/cereal_container.glb",
+                descriptionURL: "/projects/cube_001/desc.html",
+                imagesURL: "/projects/cube_001/images.html",
+                scale: {
+                    x: .005, y: .005, z: .005
                 }
             }
         ],
         groups: [
             {
-                name: "project 1", id: "project 1"
+                name: "personal", id: "personal"
             },
             {
-                name: "project 2", id: "project 2"
+                name: "classwork", id: "classwork"
             },
             {
-                name: "project 3", id: "project 3"
+                name: "beta", id: "beta"
             },
-            {
-                name: "project 4", id: "project 4"
-            },
-            {
-                name: "project 5", id: "project 5"
-            },
-            {
-                name: "project 6", id: "project 6"
-            },
-            {
-                name: "project 7", id: "project 7"
-            },
-            {
-                name: "project 8", id: "project 8"
-            },
-            {
-                name: "project 9", id: "project 9"
-            },
+            
         ]
     }
 }
