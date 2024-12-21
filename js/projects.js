c4b2797dc84c252b93ffff9eeaa231ac91dc0226
Addison Prairie <115478618+AddisonPrairie@users.noreply.github.com>
2024-12-20 16:08:04 -0800

    a

--- js/projects.js
+++ js/projects.js
@@ -132,7 +132,6 @@ export function getProjectList() {
                 name: "professional", id: "professional",
                 preferredOrder: []
             },
-            
         ]
     }
 }
